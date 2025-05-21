210e70af7dee257463ce61698e8b897d6a44c729
Daniel Biacan III <37794476+deymdaniel@users.noreply.github.com>
2025-05-21 21:06:51 +0800

    Update demo.php

--- demo.php
+++ demo.php
@@ -1,61 +1,61 @@
 <?php
 
-/////even or odd/////
-// $num = readline("Enter a number: ");
-// $result = "Odd";
-// if ($num % 2 == 0) {
-//     $result = "Even";
-// }
-// echo "The number is {$result}";
+///even or odd/////
+$num = readline("Enter a number: ");
+$result = "Odd";
+if ($num % 2 == 0) {
+    $result = "Even";
+}
+echo "The number is {$result}\n";
 
-//////prime or not prime/////
-// function isPrimeNumber($number)
-// {
-//     if ($number < 2) {
-//         return "{$number} is not a prime\n";
-//     }
-//     if ($number == 2) {
-//         return "{$number} is prime\n";
-//     }
-//     if ($number % 2 == 0) {
-//         return "{$number} is not prime\n";
-//     }
+////prime or not prime/////
+function isPrimeNumber($number)
+{
+    if ($number < 2) {
+        return "{$number} is not a prime\n";
+    }
+    if ($number == 2) {
+        return "{$number} is prime\n";
+    }
+    if ($number % 2 == 0) {
+        return "{$number} is not prime\n";
+    }
 
-//     $sqrtNumber = sqrt($number);
-//     for ($i = 3; $i <= $sqrtNumber; $i += 2) {
-//         if ($number % $i == 0) {
-//             return "{$number} is not prime\n";
-//         }
-//     }
+    $sqrtNumber = sqrt($number);
+    for ($i = 3; $i <= $sqrtNumber; $i += 2) {
+        if ($number % $i == 0) {
+            return "{$number} is not prime\n";
+        }
+    }
 
-//     return "{$number} is prime\n";
-// }
+    return "{$number} is prime\n";
+}
 
-// $number = readline("Enter a number: ");
-// if (!is_numeric($number)) {
-//     echo "Invalid input!";
-//     exit(1);
-// }
+$number = readline("Enter a number: ");
+if (!is_numeric($number)) {
+    echo "Invalid input!";
+    exit(1);
+}
 
-// $number = (int) $number;
-// for ($i = 0; $i <= $number; $i++) {
-//     echo isPrimeNumber($i);
-// }
+$number = (int) $number;
+for ($i = 0; $i <= $number; $i++) {
+    echo isPrimeNumber($i);
+}
 
 require __DIR__ . "/vendor/autoload.php";
 
 use Cocur\Slugify\Slugify;
-use ramsey\Uuid\Uuid;
+use Ramsey\Uuid\Uuid;
+use demi\timelog\TimeLog;
 
 $sluggify = new Slugify();
-echo $sluggify->slugify("Hello World") . "\n";
+echo $slugify.slugify("This is the new sample text.");
 
-$uuid = Uuid::uuid4();
-echo $uuid->toString();
+$uuid = Uuid->uuid4();
+echo $uuid.toString();
 
-$faker = Faker\Factory::create();
-echo $faker->name() . "\n";
-echo $faker->email() . "\n";
-echo $faker->phoneNumber() . "\n";
-echo $faker->city() . "\n";
-echo $faker->state() . "\n";
+$faker = Faker\Factory->create();
+echo $faker.name();
+echo $faker.email();
+echo $faker.text();
+
